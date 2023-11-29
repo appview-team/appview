c605415159f4f4c0418c0bf4b6f4b5fca9cffeaf
Donn <donnrochette@gmail.com>
2023-11-29 15:18:52 -0600

    Fixed notify env var names to be consistent with others.

--- src/notify.h
+++ src/notify.h
@@ -1,8 +1,8 @@
 #ifndef __NOTIFY_H__
 #define __NOTIFY_H__
 
-#define SLACK_TOKEN_VAR "SLACKBOT_TOKEN"
-#define SLACK_CHANNEL "SLACKBOT_CHANNEL"
+#define SLACK_TOKEN_VAR "SCOPE_SLACKBOT_TOKEN"
+#define SLACK_CHANNEL "SCOPE_SLACKBOT_CHANNEL"
 #define SLACK_API_HOST "slack.com"
 #define SLACK_API_PATH "/api/chat.postMessage"
 #define SLACK_CHANNEL_ID "#notifications"
